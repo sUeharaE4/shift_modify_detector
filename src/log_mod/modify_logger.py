e0ffb211c6a241aeb15a820fb56c0cd3818d682e
ShunichiUehara <funa1hasi2hasi@gmail.com>
2019-05-13 00:34:07 +0900

    Fix : create_logger to update level if logger already exist

--- src/log_mod/modify_logger.py
+++ src/log_mod/modify_logger.py
@@ -6,13 +6,22 @@ loggers = {}
 class ModifyLogger:
 
     @classmethod
+    def setLevelUtil(cls, logger, level):
+        logger.setLevel(level)
+        for handler in logger.handlers:
+            handler.setLevel(level)
+        return logger
+
+    @classmethod
     def create_logger(cls, name=None, log_level=INFO):
         global loggers
         if name is None:
             name = __name__
 
         if loggers.get(name):
-            return loggers.get(name)
+            logger = loggers.get(name)
+            logger = cls.setLevelUtil(logger, log_level)
+            return logger
 
         handler = StreamHandler()
         formatter = \
@@ -24,11 +33,4 @@ class ModifyLogger:
         handler.setLevel(log_level)
         logger.addHandler(handler)
         loggers[name] = logger
-        return logger
-
-    @classmethod
-    def setLevelUtil(cls, logger, level):
-        logger.setLevel(level)
-        for handler in logger.handlers:
-            handler.setLevel(level)
         return logger
