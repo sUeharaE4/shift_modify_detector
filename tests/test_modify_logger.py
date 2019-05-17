c69f7c7d54b08ae370076c6846be719453c1334e
ShunichiUehara <funa1hasi2hasi@gmail.com>
2019-05-18 03:03:41 +0900

    Update : add test case

--- tests/test_modify_logger.py
+++ tests/test_modify_logger.py
@@ -23,6 +23,8 @@ def test_setLevelUtil(log_level):
 def test_create_logger(log_level):
     modify_logger_cls = modify_logger.ModifyLogger()
     logger = modify_logger_cls.create_logger(__name__, log_level)
+    logger2 = modify_logger_cls.create_logger(log_level=log_level)
+    assert logger.name != logger2.name
     assert logger.level == log_level
     for handler in logger.handlers:
         assert handler.level == log_level
