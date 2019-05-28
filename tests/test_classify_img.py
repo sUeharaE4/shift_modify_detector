4d3123e3aee2cee98d9c7da2c7e5690e9bb6ebfe
ShunichiUehara <funa1hasi2hasi@gmail.com>
2019-05-29 01:00:01 +0900

    Add : test for img_classifer

---
+++ tests/test_classify_img.py
@@ -0,0 +1,58 @@
+import numpy as np
+import argparse
+import pytest
+import os
+import json
+import cv2
+import time
+import sys
+import pandas as pd
+sys.path.append(os.getcwd())
+sys.path.append('../src/')
+sys.path.append('../src/log_mod/')
+sys.path.append('../src/image_classifer/')
+
+import util
+import classify_img
+
+TEST_TMP_DIR = 'test_tmp'
+DIR_SEP = os.sep
+
+
+@pytest.mark.parametrize('conf_path, input_args', [
+    ('conf/classify_default.yml', {'img_path': None, 'classify_multi': False}),
+    ('conf/classify_default.yml',  {'score_path': 'XXX'}),
+    ('conf/classify_default.yml', {'classify_multi': True, 'classify_dir': 'XXX'}),
+    ('conf/classify_default.yml', {'change_bright': True, 'threthold_W': None}),
+    ('conf/classify_default.yml', {'change_bright': True, 'threthold_B': None}),
+    ('conf/classify_default.yml', {}),
+])
+def test_check_config(conf_path, input_args):
+    args = argparse.Namespace()
+    args.__dict__ = input_args
+    args.conf_path = conf_path
+    config = util.get_config(args)
+    if len(input_args) > 0:
+        assert not classify_img.check_config(config)
+    else:
+        assert classify_img.check_config(config)
+
+
+@pytest.mark.parametrize('unknown_path, known_path, match_dict, expect', [
+    ('input/0001.jpg', 'input/template.jpg', {'input/0001.jpg': {'input/template.jpg': 3.14}}, 3.14),
+    ('input/0001.jpg', 'input/0001.jpg', {'input/0001.jpg': {'input/template.jpg': 3.14}}, 0.0),
+    ('input/0001.jpg', 'images/mnist_7.png', {}, 10000000),
+    ('input/0001.jpg', 'input/0001.jpg', {}, 0.0),
+])
+def test_get_match_points(unknown_path, known_path, match_dict, expect):
+    assert classify_img.get_match_points(unknown_path, known_path, match_dict) == expect
+
+
+@pytest.mark.parametrize('match_dict, expect_csv_path', [
+    ({'input/0001.jpg': {'input/template.jpg': 3.14},
+      'input/XXX.jpg': {'input/template.jpg': 2.718}}, 'image_classify/result.csv'),
+])
+def test_create_result_df(match_dict, expect_csv_path):
+    expect_df = pd.read_csv(expect_csv_path).reset_index(drop=True)
+    result_df = classify_img.create_result_df(match_dict).reset_index(drop=True)
+    assert (result_df == expect_df).all().all()
